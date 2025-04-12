a09fb7886aac88334b9543a89c568a63373f1292
karthik-ak-dev <karthikareon@gmail.com>
2025-04-12 09:50:37 +0530

    Kustom pallete logic updated with TODO code -- Working as expected

--- pallets/risk-ratings/src/lib.rs
+++ pallets/risk-ratings/src/lib.rs
@@ -1,202 +1,235 @@
-//! # Template Pallet
+//! # Todo List Pallet
 //!
-//! A pallet with minimal functionality to help developers understand the essential components of
-//! writing a FRAME pallet. It is typically used in beginner tutorials or in Substrate template
-//! nodes as a starting point for creating a new pallet and **not meant to be used in production**.
+//! A simple todo list pallet that demonstrates the essential components of
+//! writing a FRAME pallet. This pallet allows users to create, complete, and delete todo items.
 //!
 //! ## Overview
 //!
-//! This template pallet contains basic examples of:
-//! - declaring a storage item that stores a single `u32` value
-//! - declaring and using events
-//! - declaring and using errors
-//! - a dispatchable function that allows a user to set a new value to storage and emits an event
-//!   upon success
-//! - another dispatchable function that causes a custom error to be thrown
+//! This pallet implements basic todo list functionality:
+//! - Creating new todo items
+//! - Marking todo items as complete
+//! - Deleting todo items
+//! - Storing todo items with their completion status
+//! - Emitting events for todo operations
+//! - Handling errors for invalid operations
 //!
-//! Each pallet section is annotated with an attribute using the `#[pallet::...]` procedural macro.
-//! This macro generates the necessary code for a pallet to be aggregated into a FRAME runtime.
+//! ## Technical Details
 //!
-//! Learn more about FRAME macros [here](https://docs.substrate.io/reference/frame-macros/).
+//! ### Storage
+//! - Uses a StorageMap to store todos with u32 keys
+//! - Uses a StorageValue to maintain the next available todo ID
 //!
-//! ### Pallet Sections
+//! ### Security
+//! - Only the owner of a todo can modify or delete it
+//! - Title length is bounded to prevent storage abuse
+//! - ID counter checks for overflow
 //!
-//! The pallet sections in this template are:
-//!
-//! - A **configuration trait** that defines the types and parameters which the pallet depends on
-//!   (denoted by the `#[pallet::config]` attribute). See: [`Config`].
-//! - A **means to store pallet-specific data** (denoted by the `#[pallet::storage]` attribute).
-//!   See: [`storage_types`].
-//! - A **declaration of the events** this pallet emits (denoted by the `#[pallet::event]`
-//!   attribute). See: [`Event`].
-//! - A **declaration of the errors** that this pallet can throw (denoted by the `#[pallet::error]`
-//!   attribute). See: [`Error`].
-//! - A **set of dispatchable functions** that define the pallet's functionality (denoted by the
-//!   `#[pallet::call]` attribute). See: [`dispatchables`].
-//!
-//! Run `cargo doc --package pallet-template --open` to view this pallet's documentation.
+//! ### Events
+//! Emits events for:
+//! - Todo creation
+//! - Todo completion
+//! - Todo deletion
 
-// We make sure this pallet uses `no_std` for compiling to Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-// Re-export pallet items so that they can be accessed from the crate namespace.
 pub use pallet::*;
 
-// FRAME pallets require their own "mock runtimes" to be able to run unit tests. This module
-// contains a mock runtime specific for testing this pallet's functionality.
-#[cfg(test)]
-mod mock;
-
-// This module contains the unit tests for this pallet.
-// Learn about pallet unit testing here: https://docs.substrate.io/test/unit-testing/
-#[cfg(test)]
-mod tests;
-
-// Every callable function or "dispatchable" a pallet exposes must have weight values that correctly
-// estimate a dispatchable's execution time. The benchmarking module is used to calculate weights
-// for each dispatchable and generates this pallet's weight.rs file. Learn more about benchmarking here: https://docs.substrate.io/test/benchmark/
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
 pub mod weights;
 pub use weights::*;
 
-// All pallet logic is defined in its own module and must be annotated by the `pallet` attribute.
 #[frame_support::pallet]
 pub mod pallet {
-	// Import various useful types required by all FRAME pallets.
-	use super::*;
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use sp_std::prelude::*; // This brings Vec into scope
 
-	// The `Pallet` struct serves as a placeholder to implement traits, methods and dispatchables
-	// (`Call`s) in this pallet.
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    /// Struct representing a single todo item
+    ///
+    /// Properties:
+    /// - title: The todo item's text (bounded to 100 bytes for storage efficiency)
+    /// - completed: Boolean flag indicating if the todo is done
+    /// - owner: The account that created this todo
+    #[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+    pub struct Todo<AccountId> {
+        /// The title of the todo item, limited to 100 bytes
+        pub title: BoundedVec<u8, ConstU32<100>>,
+        /// Whether the todo is completed
+        pub completed: bool,
+        /// The account that owns this todo
+        pub owner: AccountId,
+    }
 
-	/// The pallet's configuration trait.
-	///
-	/// All our types and constants a pallet depends on must be declared here.
-	/// These types are defined generically and made concrete when the pallet is declared in the
-	/// `runtime/src/lib.rs` file of your chain.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// The overarching runtime event type.
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		/// A type representing the weights required by the dispatchables of this pallet.
-		type WeightInfo: WeightInfo;
-	}
+    /// The pallet's configuration trait
+    /// This is where we declare types and constants that the pallet depends on.
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-	/// A storage item for this pallet.
-	///
-	/// In this template, we are declaring a storage item called `Something` that stores a single
-	/// `u32` value. Learn more about runtime storage here: <https://docs.substrate.io/build/runtime-storage/>
-	#[pallet::storage]
-	pub type Something<T> = StorageValue<_, u32>;
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The type that will be used for Event variants
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        /// Type for weight information
+        type WeightInfo: WeightInfo;
+    }
 
-	/// Events that functions in this pallet can emit.
-	///
-	/// Events are a simple means of indicating to the outside world (such as dApps, chain explorers
-	/// or other users) that some notable update in the runtime has occurred. In a FRAME pallet, the
-	/// documentation for each event field and its parameters is added to a node's metadata so it
-	/// can be used by external interfaces or tools.
-	///
-	///	The `generate_deposit` macro generates a function on `Pallet` called `deposit_event` which
-	/// will convert the event type of your pallet into `RuntimeEvent` (declared in the pallet's
-	/// [`Config`] trait) and deposit it using [`frame_system::Pallet::deposit_event`].
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// A user has successfully set a new value.
-		SomethingStored {
-			/// The new value set.
-			something: u32,
-			/// The account who set the new value.
-			who: T::AccountId,
-		},
-	}
+    /// Main storage for todos
+    ///
+    /// This is a mapping from todo ID (u32) to Todo struct.
+    /// We use Blake2_128Concat as the hasher for good performance and security.
+    #[pallet::storage]
+    pub type Todos<T: Config> = StorageMap<
+        _,                  // Prefix (automatically set by the system)
+        Blake2_128Concat,   // Hasher
+        u32,                // Key (todo ID)
+        Todo<T::AccountId>, // Value (todo struct)
+    >;
 
-	/// Errors that can be returned by this pallet.
-	///
-	/// Errors tell users that something went wrong so it's important that their naming is
-	/// informative. Similar to events, error documentation is added to a node's metadata so it's
-	/// equally important that they have helpful documentation associated with them.
-	///
-	/// This type of runtime error can be up to 4 bytes in size should you want to return additional
-	/// information.
-	#[pallet::error]
-	pub enum Error<T> {
-		/// The value retrieved was `None` as no value was previously set.
-		NoneValue,
-		/// There was an attempt to increment the value in storage over `u32::MAX`.
-		StorageOverflow,
-	}
+    /// Counter for generating unique todo IDs
+    ///
+    /// This value automatically starts at 0 thanks to ValueQuery
+    /// and is incremented each time a new todo is created.
+    #[pallet::storage]
+    pub type NextTodoId<T> = StorageValue<_, u32, ValueQuery>;
 
-	/// The pallet's dispatchable functions ([`Call`]s).
-	///
-	/// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	/// These functions materialize as "extrinsics", which are often compared to transactions.
-	/// They must always return a `DispatchResult` and be annotated with a weight and call index.
-	///
-	/// The [`call_index`] macro is used to explicitly
-	/// define an index for calls in the [`Call`] enum. This is useful for pallets that may
-	/// introduce new dispatchables over time. If the order of a dispatchable changes, its index
-	/// will also change which will break backwards compatibility.
-	///
-	/// The [`weight`] macro is used to assign a weight to each call.
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a single u32 value as a parameter, writes the value
-		/// to storage and emits an event.
-		///
-		/// It checks that the _origin_ for this call is _Signed_ and returns a dispatch
-		/// error if it isn't. Learn more about origins here: <https://docs.substrate.io/build/origins/>
-		#[pallet::call_index(0)]
-		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			let who = ensure_signed(origin)?;
+    /// Events emitted by this pallet
+    ///
+    /// Events help external entities (like apps) track what happened in the chain.
+    /// Each event includes the account that triggered it and relevant data.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Emitted when a new todo is created
+        /// Parameters: [todo_id, creator_account]
+        TodoCreated { todo_id: u32, who: T::AccountId },
+        /// Emitted when a todo is marked complete
+        /// Parameters: [todo_id, completer_account]
+        TodoCompleted { todo_id: u32, who: T::AccountId },
+        /// Emitted when a todo is deleted
+        /// Parameters: [todo_id, deleter_account]
+        TodoDeleted { todo_id: u32, who: T::AccountId },
+    }
 
-			// Update storage.
-			Something::<T>::put(something);
+    /// Custom errors that can occur in this pallet
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Returned when trying to modify a non-existent todo
+        TodoNotFound,
+        /// Returned when someone tries to modify a todo they don't own
+        NotTodoOwner,
+        /// Returned when the todo title exceeds 100 bytes
+        TitleTooLong,
+        /// Returned if we've reached the maximum number of todos (u32::MAX)
+        TodoIdOverflow,
+    }
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
+    /// Dispatchable functions (callable extrinsics)
+    /// These are the functions that users can call to interact with the pallet
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Creates a new todo item
+        ///
+        /// Parameters:
+        /// - origin: The call origin (must be signed)
+        /// - title: The title of the todo item
+        ///
+        /// Flow:
+        /// 1. Verify the caller
+        /// 2. Convert title to bounded vector
+        /// 3. Generate new todo ID
+        /// 4. Create and store todo
+        /// 5. Emit event
+        #[pallet::call_index(0)]
+        #[pallet::weight(T::WeightInfo::do_something())]
+        pub fn create_todo(origin: OriginFor<T>, title: Vec<u8>) -> DispatchResult {
+            // Ensure the call is signed and get the signer's account
+            let who = ensure_signed(origin)?;
 
-			// Return a successful `DispatchResult`
-			Ok(())
-		}
+            // Convert the title to a bounded vector, ensuring it's not too long
+            let bounded_title = BoundedVec::<u8, ConstU32<100>>::try_from(title)
+                .map_err(|_| Error::<T>::TitleTooLong)?;
 
-		/// An example dispatchable that may throw a custom error.
-		///
-		/// It checks that the caller is a signed origin and reads the current value from the
-		/// `Something` storage item. If a current value exists, it is incremented by 1 and then
-		/// written back to storage.
-		///
-		/// ## Errors
-		///
-		/// The function will return an error under the following conditions:
-		///
-		/// - If no value has been set ([`Error::NoneValue`])
-		/// - If incrementing the value in storage causes an arithmetic overflow
-		///   ([`Error::StorageOverflow`])
-		#[pallet::call_index(1)]
-		#[pallet::weight(T::WeightInfo::cause_error())]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+            // Get the next todo ID and increment it
+            let todo_id = NextTodoId::<T>::get();
+            let next_todo_id = todo_id.checked_add(1).ok_or(Error::<T>::TodoIdOverflow)?;
 
-			// Read a value from storage.
-			match Something::<T>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage. This will cause an error in the event
-					// of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::<T>::put(new);
-					Ok(())
-				},
-			}
-		}
-	}
+            // Create the new todo item
+            let todo = Todo {
+                title: bounded_title,
+                completed: false,
+                owner: who.clone(),
+            };
+
+            // Store the todo in chain storage
+            Todos::<T>::insert(todo_id, todo);
+            NextTodoId::<T>::put(next_todo_id);
+
+            // Emit the creation event
+            Self::deposit_event(Event::TodoCreated { todo_id, who });
+
+            Ok(())
+        }
+
+        /// Marks a todo as complete
+        ///
+        /// Parameters:
+        /// - origin: The call origin (must be signed)
+        /// - todo_id: The ID of the todo to complete
+        ///
+        /// Flow:
+        /// 1. Verify the caller
+        /// 2. Find and verify todo ownership
+        /// 3. Update completion status
+        /// 4. Emit event
+        #[pallet::call_index(1)]
+        #[pallet::weight(T::WeightInfo::do_something())]
+        pub fn complete_todo(origin: OriginFor<T>, todo_id: u32) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            // try_mutate allows us to modify storage in a closure
+            Todos::<T>::try_mutate(todo_id, |maybe_todo| -> DispatchResult {
+                // Get a mutable reference to the todo or return TodoNotFound
+                let todo = maybe_todo.as_mut().ok_or(Error::<T>::TodoNotFound)?;
+
+                // Verify ownership
+                ensure!(todo.owner == who, Error::<T>::NotTodoOwner);
+
+                // Mark as complete
+                todo.completed = true;
+
+                // Emit completion event
+                Self::deposit_event(Event::TodoCompleted { todo_id, who });
+                Ok(())
+            })
+        }
+
+        /// Deletes a todo item
+        ///
+        /// Parameters:
+        /// - origin: The call origin (must be signed)
+        /// - todo_id: The ID of the todo to delete
+        ///
+        /// Flow:
+        /// 1. Verify the caller
+        /// 2. Check todo exists and verify ownership
+        /// 3. Remove from storage
+        /// 4. Emit event
+        #[pallet::call_index(2)]
+        #[pallet::weight(T::WeightInfo::do_something())]
+        pub fn delete_todo(origin: OriginFor<T>, todo_id: u32) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            // Get the todo and verify ownership
+            let todo = Todos::<T>::get(todo_id).ok_or(Error::<T>::TodoNotFound)?;
+            ensure!(todo.owner == who, Error::<T>::NotTodoOwner);
+
+            // Remove the todo from storage
+            Todos::<T>::remove(todo_id);
+
+            // Emit deletion event
+            Self::deposit_event(Event::TodoDeleted { todo_id, who });
+
+            Ok(())
+        }
+    }
 }
